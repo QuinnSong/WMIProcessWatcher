3853925a9379dfac9bacb797e6b04efc3d56a3a0
Qinglin-Song <Qinglin.Song@coxautoinc.com>
2021-11-21 22:53:00 -0500

    add watch for ie browser

--- WMIProcessWatcher/Program.cs
+++ WMIProcessWatcher/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Management;
 using System.Security.Principal;
 using System.Threading;
@@ -8,11 +10,14 @@ namespace WMI
 {
     class Program
     {
+        private static Dictionary<int, TimeSpan> ProcessPool;
         static void Main(string[] args)
         {
             RunThisAsAdmin();
             new Thread(WaitForProcess) { IsBackground = true, Name = "worker" }.Start();
             Console.WriteLine("Waiting for process events");
+            ProcessPool = new Dictionary<int, TimeSpan>();
+
             do
             {
                 Thread.Sleep(5000);
@@ -79,13 +84,64 @@ namespace WMI
             {
                 var proc = GetProcessInfo(e);
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("+ {0} ({1}) {2} [{3}]", proc.ProcessName, proc.PID, proc.CommandLine, proc.User);
+                var output = (proc.ProcessName + "|" + proc.CommandLine).ToLower();
+                if (output.Contains("iexplore.exe") || output.Contains("IEDriverServer".ToLower()))
+                {
+                    if (ProcessPool.ContainsKey(proc.PID))
+                    {
+                        var time = TimeSpan.FromTicks(DateTime.Now.Ticks).Subtract(ProcessPool[proc.PID]);
+                        if (time.TotalMinutes > 5)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            KillProcessById(proc.PID);
+                            Console.WriteLine("!!!! 5 min timeout reached. Killed {0} ({1}) {2} [{3}]", proc.ProcessName, proc.PID, proc.CommandLine, proc.User);
+                        }
+                    }
+                    else
+                    {
+                        ProcessPool[proc.PID] = TimeSpan.FromTicks(DateTime.Now.Ticks);
+                        Console.WriteLine("+ {0} ({1}) {2} [{3}]", proc.ProcessName, proc.PID, proc.CommandLine, proc.User);
+                    }                    
+                }
                 //            Console.WriteLine("+ {0} ({1}) {2} > {3} ({4}) {5}", proc.ProcessName, proc.PID, proc.CommandLine, pproc.ProcessName, pproc.PID, pproc.CommandLine);
             }
             catch (Exception ex)
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine(ex);
+            }
+        }
+
+        private static bool ProcessExists(int id) => Process.GetProcesses().Any(x => x.Id == id);
+
+        /// <summary>
+        /// Kill a process by PID.
+        /// </summary>
+        /// <param name="pid">Process ID.</param>
+        private static void KillProcessById(int pid)
+        {
+            // Cannot close 'system idle process'.
+            if (pid == 0)
+            {
+                return;
+            }
+            try
+            {
+                Process proc = Process.GetProcessById(pid);
+                proc.Kill();
+            }
+            catch (Exception e)
+            {
+                // Process already exited.
+                Console.WriteLine($">>> Trace Log: {e.StackTrace}");
+
+            }
+            finally
+            {
+                if (!ProcessExists(pid))
+                {
+                    ProcessPool.Remove(pid);
+                }
             }
         }
 
